ef705b85adc2ce3c027322c027b0fc4d7fc96aa9
Carlton Joseph <carlton.joseph@gmail.com>
2020-03-14 21:12:32 -0700

    initial d3 component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { select } from "d3";
+
+import "./App.css";
 
 function App() {
+  const [data, dataSet] = React.useState([25, 30, 45, 50, 20]);
+  const svgRef = React.useRef();
+
+  React.useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", v => v)
+      .attr("cx", v => v * 2)
+      .attr("cy", v => v * 2)
+      .attr("stroke", "red");
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <svg ref={svgRef}></svg>
+      <hr />
+      <button onClick={() => dataSet(data.map(v => v + 5))}>Update Data</button>
+      <button onClick={() => dataSet(data.filter(v => v < 35))}>
+        Filter Data
+      </button>
+    </>
   );
 }
 
