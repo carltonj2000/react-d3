d055e7f996097c1f5d43d9de990b66c8a198e962
Carlton Joseph <carlton.joseph@gmail.com>
2020-03-14 22:16:32 -0700

    add line graph

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from "react";
-import { select } from "d3";
+import { select, line, curveCardinal } from "d3";
 
 import "./App.css";
 
+import Basics from "./Basics";
+
 function App() {
-  const [data, dataSet] = React.useState([25, 30, 45, 50, 20]);
+  const [data, dataSet] = React.useState([25, 30, 45, 50, 20, 65, 75]);
   const svgRef = React.useRef();
 
   React.useEffect(() => {
     const svg = select(svgRef.current);
+    const myLine = line()
+      .x((v, i) => i * 50)
+      .y(v => 150 - v)
+      .curve(curveCardinal);
     svg
-      .selectAll("circle")
-      .data(data)
-      .join("circle")
-      .attr("r", v => v)
-      .attr("cx", v => v * 2)
-      .attr("cy", v => v * 2)
-      .attr("stroke", "red");
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", v => myLine(data))
+      .attr("fill", "none")
+      .attr("stroke", "blue");
   }, [data]);
 
+  const tutorial = 0;
+  if (tutorial === 1) return <Basics />;
   return (
     <>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        {/* <path d="M0,150 100,100 150,120" stroke="blue" fill="none" /> */}
+      </svg>
       <hr />
       <button onClick={() => dataSet(data.map(v => v + 5))}>Update Data</button>
       <button onClick={() => dataSet(data.filter(v => v < 35))}>
---
+++ src/Basics.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { select } from "d3";
+
+export default function Basics() {
+  const [data, dataSet] = React.useState([25, 30, 45, 50, 20]);
+  const svgRef = React.useRef();
+
+  React.useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", v => v)
+      .attr("cx", v => v * 2)
+      .attr("cy", v => v * 2)
+      .attr("stroke", "red");
+  }, [data]);
+
+  return (
+    <>
+      <svg ref={svgRef}></svg>
+      <hr />
+      <button onClick={() => dataSet(data.map(v => v + 5))}>Update Data</button>
+      <button onClick={() => dataSet(data.filter(v => v < 35))}>
+        Filter Data
+      </button>
+    </>
+  );
+}
